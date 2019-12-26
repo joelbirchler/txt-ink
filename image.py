d42aafaa283aaa7cb3cb562d9f5e24dab0ebb1f5
Joel Birchler <joel@joelbirchler.com>
2019-12-25 22:23:27 -0800

    turns off dithering

--- image.py
+++ image.py
@@ -7,7 +7,7 @@ from inky import InkyWHAT, InkyPHAT
 
 imagePath = sys.argv[1]
 img = Image.open(imagePath)
-img = img.convert("1")
+img = img.convert("1", dither = Image.NONE)
 
 display = InkyPHAT("black")
 display.set_image(img)
